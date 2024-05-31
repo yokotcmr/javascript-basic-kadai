bb561ae2b96cf7fdf3e9821c39c88613800c7d00
yokotcmr <166018980+yokotcmr@users.noreply.github.com>
2024-05-31 21:54:04 +0900

    kadai_014提出

---
+++ kadai_014/square.js
@@ -0,0 +1,10 @@
+function calculateSquare(number) {
+    
+    const square = number * number;
+  
+        return square;
+  }
+  
+  // 関数を実行して結果をコンソールに出力
+  const result = calculateSquare(10);
+  console.log(result); 
